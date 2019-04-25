794d2dd4faa56823f4ff454ba41822a89490716c
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-25 15:37:44 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = parseInt(time);
+  if (hour < 12){
+    return 'Good Morning';
+  } else if (hour < 18) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerHTML = str;
+}
