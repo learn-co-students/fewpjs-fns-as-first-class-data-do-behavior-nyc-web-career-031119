1a5e2020cb0e182de0731bf045ea4da19f1b153e
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-04-24 20:32:05 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let message
+  const timeInt = parseInt(time)
+  switch (true){
+    case (timeInt < 12):
+    message =  "Good Morning"
+    break
+    case (timeInt < 17 && timeInt > 11):
+    message =  "Good Afternoon"
+    break
+    default:
+    message =  "Good Evening"
+  }
+  return message
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  const greeting = document.querySelector("#greeting")
+  greeting.innerHTML = string
+}
